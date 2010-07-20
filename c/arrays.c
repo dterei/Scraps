ac23e16e6b12d4533793ff4a0c4e9b8af5899705
David Terei <davidterei@gmail.com>
2010-07-20 14:48:34 +0100

    Update c/arrays.c to incldue some function pointer functions.

--- c/arrays.c
+++ c/arrays.c
@@ -2,6 +2,16 @@
 
 int my_array[] = {1,2,3,4,5,6};
 
+void f()
+{
+	printf("called f\n");
+}
+
+void (*f_pointer())(void)
+{
+	return &f;
+}
+
 int hundredth(int a[])
 {
 	return a[100];
@@ -10,6 +20,11 @@ int hundredth(int a[])
 int head(int a[])
 {
 	return a[0];
+}
+
+int (*f2())(int*)
+{
+	return &head;
 }
 
 int previous(int a[])
@@ -21,8 +36,12 @@ int main(void)
 {
 	int one  = head(my_array);
 	int prev = previous(&my_array[1]);
+	int two = f2()(my_array);
+	f();
+	f_pointer()();
 	printf("Head of array => %d\n", one);
 	printf("Head of array => %d\n", prev);
+	printf("Head of array => %d\n", two);
 	return 0;
 }
 
