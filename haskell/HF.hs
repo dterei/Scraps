7fd85b15c95aeb60236492ce36bc75c251b0d188
David Terei <davidterei@gmail.com>
2010-08-17 13:20:55 +1000

    Add big arity fun test.

---
+++ haskell/HF.hs
@@ -0,0 +1,8 @@
+module HugeFun where
+
+hugeFun :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> 
+           Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> 
+           Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int
+hugeFun a b c d e f g h i j k l m n o p q r s t u v w x y z
+    = sum [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+
---
+++ haskell/HugeFun.hs
@@ -0,0 +1,6 @@
+module HugeFun where
+
+hugeFun :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int
+hugeFun a b c d e f g
+    = sum [a,b,c,d,e,f,g]
+
