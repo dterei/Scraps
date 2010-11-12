78513504a10929f865f79a495e59a3b3b53f6370
David Terei <davidterei@gmail.com>
2010-11-12 16:02:20 +1100

    Modified HugeFun hs test.

--- haskell/HugeFun.hs
+++ haskell/HugeFun.hs
@@ -2,5 +2,7 @@ module HugeFun where
 
 hugeFun :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int
 hugeFun a b c d e f g
-    = sum [a,b,c,d,e,f,g]
+    = if a == 1
+         then sum [a,b,c,d,e,f,g]
+         else sum [-a,b,c,d,e,f,g*2]
 
