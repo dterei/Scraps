acb3ded45cb26f89a1626717053a31d1b409fba0
David Terei <davidterei@gmail.com>
2011-08-09 12:01:37 -0700

    Add Haskell __GLASGOW_HASKELL__ test

---
+++ haskell/ghcCPP.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE CPP #-}
+module Main where
+
+main = putStrLn $ "Testing __GLASGOW_HASKELL__: " ++ ver
+
+ver :: String
+#if __GLASGOW_HASKELL__ >= 701
+ver = "701"
+#elif __GLASGOW_HASKELL__ >= 700
+ver = "700"
+#elif __GLASGOW_HASKELL__ >= 699
+ver = "699"
+#elif __GLASGOW_HASKELL__ >= 698
+ver = "698"
+#elif __GLASGOW_HASKELL__ >= 697
+ver = "697"
+#elif __GLASGOW_HASKELL__ >= 680
+ver = "680"
+#elif __GLASGOW_HASKELL__ >= 613
+ver = "613"
+#elif __GLASGOW_HASKELL__ >= 612
+ver = "612"
+#elif __GLASGOW_HASKELL__ >= 611
+ver = "611"
+#elif __GLASGOW_HASKELL__ >= 610
+ver = "610"
+#elif __GLASGOW_HASKELL__ >= 608
+ver = "608"
+#else
+ver = "unknown"
+#endif
+
