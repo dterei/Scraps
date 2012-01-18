ff2ea52c1ce8f78f4bcdffb95c75c9ec0b73cbd2
David Terei <davidterei@gmail.com>
2012-01-17 19:05:19 -0800

    Ruby hardway work.

---
+++ ruby/hardway/ex2.rb
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later.
+# Anything after the # character is ignored by Ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
+
---
+++ ruby/hardway/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:"
+
+puts "Hens", 25 + 30 / 6 # ? 30
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4  + 6 # (1 + 0 - 1/4 + 6) = 7 - 1 / 4 = 6.75 ? types
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 # false
+
+puts "What is 3 + 2?", 3 + 2, "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it is false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
