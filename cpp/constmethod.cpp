fe6538c624df26cf2d7537e2c99723ac53eba8aa
David Terei <code@davidterei.com>
2014-10-14 17:35:22 -0700

    play with cpp const methods

---
+++ cpp/constmethod.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace  std;
+
+class MyClass
+{
+private:
+  // if wasn't mutable, const method can't access
+  mutable int counter;
+  int myid;
+public:
+  MyClass() : counter(0), myid(0) {}
+
+  void Foo()
+  {
+    counter++;
+    cout << "Foo" << endl;    
+  }
+
+  void Foo() const
+  {
+    counter++;
+    // Goo();
+    cout << "Foo const" << endl;
+  }
+
+  void Goo()
+  {
+    myid = 12;
+  }
+
+  int GetInvocations() const
+  {
+    return counter;
+  }
+};
+
+int main(void)
+{
+  MyClass* cc = new MyClass();
+  const MyClass* ccc = cc;
+  cc->Foo();
+  ccc->Foo();
+  cout
+    << "The MyClass instance has been invoked" << ccc->GetInvocations()
+    << " times" << endl; 
+  delete cc;
+  ccc = NULL;
+  return 0;
+}
+
