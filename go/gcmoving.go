c8aa6aeb34d6b7016c56c25197e993aab65e3bde
David Terei <code@davidterei.com>
2016-04-25 22:15:15 -0700

    Update to golang gcmoving / pointer test.

--- go/gcmoving.go
+++ go/gcmoving.go
@@ -1,6 +1,12 @@
 // Can we operate over pointer values? i.e., the address of memory? This would
 // create issues should Go ever move to a moving GC.
 //
+// No, we can only perform equality operations, and so even for a moving
+// collector this is fine.
+//
+// One caveat is that we can serialize a pointer value to a string, this
+// serialized value will differ when memory location differs.
+//
 package main
 
 // Pointer operations
@@ -15,9 +21,13 @@ func main() {
 	x := 0
 	y := 0
 
-	fmt.Println("X: ", x, "&X:", &x)
-	fmt.Println("Y: ", x, "&Y:", &x)
-	fmt.Println("X==Y: ", x==y, "&X==&Y:", &x==&y)
+	fmt.Println("X:", x, "&X:", &x)
+	fmt.Println("Y:", x, "&Y:", &x)
+	fmt.Println("X==Y:", x==y, "&X==&Y:", &x==&y)
 	z := &x
-	fmt.Println("Z: ", z, "&Z:", &z)
+	fmt.Println("Z:", z, "&Z:", &z)
+
+	xs := fmt.Sprintf("%v", &x)
+	ys := fmt.Sprintf("%v", &y)
+	fmt.Println("X:", xs, "Y:", ys)
 }
